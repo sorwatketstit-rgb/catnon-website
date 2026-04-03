f5da5de2cfece79efd6e583a61c0180cf45b6ced
Sorawat <sorwatketstit@gmail.com>
2026-04-03 20:17:34 +0700

    add new cat image

--- script.js
+++ script.js
@@ -30,14 +30,23 @@ function startTyping() {
     }
 }
 
-// Choice 1 → Rickroll
+// Choice 1 -> Rickroll
 document.getElementById("choice1").onclick = () => {
     window.location.href =
         "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
 };
 
-// Choice 2 → show big image
+// Choice 2 -> Cat
 document.getElementById("choice2").onclick = () => {
-    document.body.innerHTML =
-        '<img src="image.jpg" style="width:100%">';
+
+    // hide text and buttons
+    text.style.display = "none";
+    choices.style.display = "none";
+
+    // change to BIG image
+    image.src = "mikhao.jpg";
+
+    // make it fullscreen
+    image.style.width = "100%";
+    image.style.marginTop = "0";
 };
